96e3165f78a64fee86c87320335a41745c6de4f2
gowalk772 <90950023+gowalk772@users.noreply.github.com>
2021-09-23 09:25:21 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+let result = []; // 最終的な二次元配列を入れるための配列
 
 //CSVファイルを読み込む関数getCSV()の定義
 function getCSV(symbol){
@@ -14,10 +14,11 @@ function getCSV(symbol){
 
 // 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
 function convertCSVtoArray(str){ // 読み込んだCSVデータが文字列として渡される
-  let result = []; // 最終的な二次元配列を入れるための配列
+  
   let tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
 
   // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
+  result = [];
   for(let i=0;i<tmp.length;++i){
       result[i] = tmp[i].split(',');
   }
@@ -58,6 +59,11 @@ ticker.onchange = function(){
   getCSV(this.value);
 }
 
+let cal = document.getElementById("cal");
+ticker.onclick = function(){
+  alert("計算");
+  result[1][0]; 
+}
 
 
 
